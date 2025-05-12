0d3cb72ec38c73673ef7204d7f5d12db0ca11780
Ashish Jain <ashishjain0338@gmail.com>
2025-05-12 18:03:03 +0530

    Adding Class for Calculator and Reading raw_material from json

--- planner/calculators/speed_and_machine_count_calculator.py
+++ planner/calculators/speed_and_machine_count_calculator.py
@@ -1,47 +1,83 @@
 import pandas as pd
 import math
 
-RAW_MATERIAL = ["copper_ore", "iron_ore", "water", "crude_oil", "coal", "stone"]
+# self.raw_material = ["copper_ore", "iron_ore", "water", "crude_oil", "coal", "stone"]
 
-def calculate_speed(item, speed_req, recipes, out, tabs):
-    # print(f"TabLevel : {tabs}  :Running for item ", item, " Speed: ", speed_req)
-    # tabStr = ""
-    if item not in recipes.keys():
-        if item not in RAW_MATERIAL:
-            print(f"WARNING: Item {item} not found")
-            exit(101)
-        return
-    if item in RAW_MATERIAL:
-        return
-        
+class SpeedAndQuantityCalculator:
 
-    item_recipe = recipes[item]
-    speed_req_per_unit = speed_req/item_recipe["unit"]
-    for ingredients, count in item_recipe["req"].items():
-        # print(f"Ingredients Level: {tabs}, = {ingredients}")
-        speed_for_ingredient = count*speed_req_per_unit
-        if ingredients in out.keys():
-            out[ingredients] += speed_for_ingredient
-        else:
-            out[ingredients] = speed_for_ingredient
-        calculate_speed(ingredients, speed_for_ingredient, recipes, out, tabs + 1)
+    def __init__(self, raw_material, planet):
+        self.raw_material = raw_material
+        self.planet = planet
 
-def get_dataframe(out):
-    df = pd.DataFrame(list(out.items()), columns=['item', 'speed'])
-    df = df.sort_values(by='item')
-    return df
+    def calculate_speed(self, item, speed_req, recipes, out, tabs):
+        # print(f"TabLevel : {tabs}  :Running for item ", item, " Speed: ", speed_req)
+        # tabStr = ""
+        if item not in recipes.keys():
+            if item not in self.raw_material:
+                print(f"WARNING: Item {item} not found")
+                exit(101)
+            return
+        if item in self.raw_material:
+            return
+            
 
-def fill_machine_count(df, recipes):
-    df["machine_count"] = [0 for i in range(len(df))]
-    for index, row in df.iterrows():
-        speed_req = row["speed"] # Units required per second
-        item = row["item"]
-        if item in RAW_MATERIAL:
-            continue
-        unit_made_per_second_per_machine = recipes[item]["unit"]/ recipes[item]["time"]# units made per second
-        machine_count =  math.ceil(speed_req / unit_made_per_second_per_machine)
-        df.loc[index, "machine_count"] = machine_count
-    return df
+        item_recipe = recipes[item]
+        speed_req_per_unit = speed_req/item_recipe["unit"]
+        for ingredients, count in item_recipe["req"].items():
+            # print(f"Ingredients Level: {tabs}, = {ingredients}")
+            speed_for_ingredient = count*speed_req_per_unit
+            if ingredients in out.keys():
+                out[ingredients] += speed_for_ingredient
+            else:
+                out[ingredients] = speed_for_ingredient
+            self.calculate_speed(ingredients, speed_for_ingredient, recipes, out, tabs + 1)
+
+    def calculate_speed_and_quantity_item_wise(self, item, speed_req, quantity, recipes, out, tabs):
+        # print(f"TabLevel : {tabs}  :Running for item ", item, " Speed: ", speed_req)
+        # tabStr = ""
+        if item not in recipes.keys():
+            if item not in self.raw_material:
+                print(f"WARNING: Item {item} not found")
+                exit(101)
+            return
+        if item in self.raw_material:
+            return
+            
+
+        item_recipe = recipes[item]
+        speed_req_per_unit = speed_req/item_recipe["unit"]
+        product_unit_count = item_recipe["unit"] # How many number of products will be produced with this recipe
+        for ingredients, ingredient_count in item_recipe["req"].items():
+            # ingredient_count is How many units of ingredient are requied for this recipe
+            # print(f"Ingredients Level: {tabs}, = {ingredients}")
+            speed_for_ingredient = ingredient_count*speed_req_per_unit
+            quantity_for_ingredient = (ingredient_count/ product_unit_count ) * quantity
+            if ingredients in out.keys():
+                out[ingredients]["speed"] += speed_for_ingredient
+                out[ingredients]["quantity"] += quantity_for_ingredient
+            else:
+                out[ingredients] = {
+                    "speed": speed_for_ingredient,
+                    "quantity": quantity_for_ingredient
+                }
+            self.calculate_speed_and_quantity_item_wise(ingredients, speed_for_ingredient, quantity_for_ingredient, recipes, out, tabs + 1)
+
+    def get_dataframe(self, out):
+        df = pd.DataFrame(list(out.items()), columns=['item', 'speed'])
+        df = df.sort_values(by='item')
+        return df
+
+    def fill_machine_count(self, df, recipes):
+        df["machine_count"] = [0 for i in range(len(df))]
+        for index, row in df.iterrows():
+            speed_req = row["speed"] # Units required per second
+            item = row["item"]
+            if item in self.raw_material:
+                continue
+            unit_made_per_second_per_machine = recipes[item]["unit"]/ recipes[item]["time"]# units made per second
+            machine_count =  math.ceil(speed_req / unit_made_per_second_per_machine)
+            df.loc[index, "machine_count"] = machine_count
+        return df
 
 
 
--- planner/main.py
+++ planner/main.py
@@ -1,13 +1,31 @@
 import json
 from utils.pretty_printers import pprint_req
 from utils.utils import load_recipe_files
-from calculators.speed_and_machine_count_calculator import calculate_speed, get_dataframe, fill_machine_count
+from calculators.speed_and_machine_count_calculator import SpeedAndQuantityCalculator
 
 def generate_insights(df):
     # 1. Quantity of each Raw-Material and 
     pass
 
-def intiate_speed_and_machine_count_calculations(req, recipe_catalog):
+def intiate_speed_and_machine_count_calculations(req, recipe_catalog, calc_obj):
+    out = {}
+    for item, details in req.items():
+        print(item, details["speed"], details["quantity"])
+        S = details["speed"]
+        Q = details["quantity"]
+        if item in out.keys():
+            out[item]["speed"] += S
+            out[item]["quantity"] += Q
+        else:
+            out[item] = {
+                "speed": S,
+                "quantity": Q
+            }
+
+        calc_obj.calculate_speed_and_quantity_item_wise(item, S, Q, recipe_catalog, out, 0)
+
+    print(out)
+    return
     # In the recipes catolog, set your required items as item = custom i.e.
     custom_req = {}
     planet = "N"
@@ -36,13 +54,17 @@ def intiate_speed_and_machine_count_calculations(req, recipe_catalog):
 
 if __name__ == '__main__':
     print("Currently All recipes must be produced on same one planet")
-    recipe_catalog = load_recipe_files()
-    req_key = "main"
+    with open("static-jsons/raw_material.json", "r") as fp:
+        RAW_MATERIAL = json.load(fp)
+    print(RAW_MATERIAL)
+    recipe_catalog = load_recipe_files("static-jsons/recipes")
+    req_key = "test1"
     with open('requirement.json', 'r') as fp:
         req = json.load(fp)
         req = req[req_key]
     pprint_req(req)
 
     print("Calculating the speed")
-    intiate_speed_and_machine_count_calculations(req, recipe_catalog)
+    calc_obj = SpeedAndQuantityCalculator(RAW_MATERIAL, "nauvis")
+    intiate_speed_and_machine_count_calculations(req, recipe_catalog, calc_obj)
     
